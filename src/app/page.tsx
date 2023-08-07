77b3248a93a3deb1cce06fb0e2953540c30ccbca
Jala <raphael@jaladesign.com.au>
2023-08-07 11:53:22 +1000

    Updated readme. Changed and set fonts and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl">Hello and welcome to my WIP</h1>
+      <h1 className="text-4xl font-thin">Hello and welcome to my WIP 😊</h1>
     </main>
   )
 }
