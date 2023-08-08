48a5c2d41fe5e70c1ae8c150f97750630347c5b6
Jala <raphael@jaladesign.com.au>
2023-08-08 18:10:16 +1000

    Added svgs to be added for social links and made some more layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,65 @@ export default function Home() {
           </section>
 
           <section className="mb-28">
-            <h2 className="text-4xl mb-4">Education</h2>
-            <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all rounded flex gap-10">
-              <div className="text-gray-400">
+            <h2 className="text-4xl mb-4 font-medium">Experience</h2>
+              <a href="https://www.jaladesign.com.au/" target="_blank">
+                <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all duration-300 rounded flex gap-10 mb-1">
+                  <div className="text-gray-400 flex-shrink-0 w-1/4 text-xs">
+                    2023 — PRESENT
+                  </div>
+                    <div className="transition-all duration-300">
+                      <p className="group-hover:text-sky-300">Jala Design — Junior Web Developer</p>
+                      <p className="text-gray-400 ">Worked with a small team using PHP/WordPress to develop websites</p>
+                  </div>
+                </div>
+              </a>
+          </section>
+
+          <section className="mb-28">
+            <h2 className="text-4xl mb-4 font-medium">Education</h2>
+            <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all duration-300 rounded flex gap-10 mb-1">
+              <div className="text-gray-400 flex-shrink-0 w-1/4 text-xs">
+                2021 — PRESENT
+              </div>
+              <div className="transition-all duration-300">
+                <p className="group-hover:text-sky-300">Bachelor of Information Technology Major in Computer Science</p>
+                <p className="text-gray-400">Macquarie University</p>
+              </div>
+            </div>
+            <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all duration-300 rounded flex gap-10 mb-1">
+              <div className="text-gray-400 flex-shrink-0 w-1/4 text-xs">
                 2020 — 2020
               </div>
-              <div className="group-hover:text-sky-300 transition-all">
-                Diploma of Information Technology
+              <div className="transition-all duration-300">
+                <p className="group-hover:text-sky-300">Diploma of Information Technology</p>
+                <p className="text-gray-400">Macquarie University</p>
+              </div>
+            </div>
+            <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all duration-300 rounded flex gap-10 mb-1">
+              <div className="text-gray-400 flex-shrink-0 w-1/4 text-xs">
+                2019 — 2020
+              </div>
+              <div className="transition-all duration-300">
+                <p className="group-hover:text-sky-300">Highschool</p>
+                <p className="text-gray-400">St. Leo's Catholic College</p>
+              </div>
+            </div>
+            <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all duration-300 rounded flex gap-10 mb-1">
+              <div className="text-gray-400 flex-shrink-0 w-1/4 text-xs">
+                2011 — 2018
+              </div>
+              <div className="transition-all duration-300">
+                <p className="group-hover:text-sky-300">Elementary - Highschool</p>
+                <p className="text-gray-400">Don Bosco Academy Pampanga</p>
+              </div>
+            </div>
+            <div className="group p-3 hover:cursor-pointer hover:bg-slate-400 hover:bg-opacity-10 hover:backdrop-blur-2xl transition-all duration-300 rounded flex gap-10 mb-1">
+              <div className="text-gray-400 flex-shrink-0 w-1/4 text-xs">
+                2006 — 2011
+              </div>
+              <div className="transition-all duration-300">
+                <p className="group-hover:text-sky-300">Elementary</p>
+                <p className="text-gray-400">Westfields International School</p>
               </div>
             </div>
           </section>
