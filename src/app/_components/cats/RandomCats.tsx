9cfda7a39b2522d7fdcc44817357e98ce72dc733
Jala Raphael <raphael@jaladesign.com.au>
2023-08-16 10:03:48 +1000

    Random cat location, still have to fix hydration issues

--- src/app/_components/cats/RandomCats.tsx
+++ src/app/_components/cats/RandomCats.tsx
@@ -1,23 +1,21 @@
 'use client'
 
-import react from "react"
 import Image from "next/image"
 import { useEffect } from "react"
 
-const RandomNumber: (max: number) => number = (max: number) => {
+const RandomNumber = (max: number) =>  {
   return Math.floor(Math.random() * max);
 }
 
 
 export default function RandomCats() {
+  const rand = RandomNumber(1000)
   useEffect(() => {
-    const rand = RandomNumber(1000)
-    console.log(rand);
-
-  }, [])
+    console.log("refresh")
+  }, [rand])
   return(
     <>
-      <div className="absolute top-[500px] right-[500px]">
+      <div className={`absolute top-[500px] right-[${rand}px]`}>
         <Image alt="cat" width={50} height={50} src="/cat5.png"/>
       </div>
       {/* <div className="absolute">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Instagram from "./_components/svgs/Instagram"
 import Linkedin from "./_components/svgs/Linkedin"
 import Lefsticky from "./_components/layout/Leftsticky"
 import Rightcontent from "./_components/layout/Rightcontent"
+import RandomCats from "./_components/cats/RandomCats"
 
 export default function Home() {
   return (
